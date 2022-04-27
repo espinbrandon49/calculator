7fe586ec606c283a0dab1a31a3c1c2b7f9924d37
espinbrandon49 <102924713+espinbrandon49@users.noreply.github.com>
2022-04-26 18:39:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -54,43 +54,41 @@ const buttonDec = document.querySelector('.decimal');
 const buttonDiv = document.querySelector('.divide');
 const buttonMlt = document.querySelector('.multiply');
 
-division.textContent = `${num1} ${num2}`
-const numF = function (num) {
-  if (num1 == typeof('number')){
-    num2 += num
-    division.innerText = `${num2}`
-  } else {
-    num1 += num
-    division.innerText = `${num1}`
-  }
-}
 
+const numF1 = function (num) {
+  num1 += num
+  num1.textContent = num
+  division.textContent = `${num1}`
+  console.log('is num1')
+  console.log(num1)
+}
+//const numF2 = function (num) {
+//  num2 += num
+//  num2.textContent = num
+//  division.textContent = `${num1} ${num2}`
+//  console.log('is num2')
+//  console.log(num2)
+//}
 buttonC.onclick = () => {
   division.textContent = "" 
   num1 = ''
   num2 = ''
 }
 
-//buttonC.onclick = () => division.textContent = "" 
 buttonCE.onclick = () => division.textContent = ''
-buttonEq.onclick = () => division.innerText = '='
-button7.onclick = () => numF(7)
-button8.onclick = () => numF(8)
-button9.onclick = () => numF(9)
-buttonAdd.onclick = () => division.innerText = '+'
-button4.onclick = () => division.innerText = 4
-button5.onclick = () => division.innerText = 5
-button6.onclick = () => division.innerText = 6
-button1.onclick = () => division.innerText = 1
-button2.onclick = () => division.innerText = 2
-button3.onclick = () => division.innerText = 3
-buttonSub.onclick = () => division.innerText = '-'
-button0.onclick = () => division.innerText = 0
-buttonDec.onclick = () => division.innerText = '.'
-buttonDiv.onclick = () => division.innerText = '/'
-buttonMlt.onclick = () => division.innerText = 'x'
-
-
-
-console.log(num1)
-console.log(num2)
+buttonEq.onclick = () => division.textContent = '='
+buttonAdd.onclick = () => division.textContent = '+'
+buttonSub.onclick = () => division.textContent = '-'
+buttonDec.onclick = () => division.textContent = '.'
+buttonDiv.onclick = () => division.textContent = '/'
+buttonMlt.onclick = () => division.textContent = 'x'
+button7.onclick = () => numF1(7)
+button8.onclick = () => numF1(8)
+//button9.onclick = () => numF(9)
+//button4.onclick = () => numF(4)
+//button5.onclick = () => numF(5)
+//button6.onclick = () => numF(6)
+//button1.onclick = () => numF(1)
+//button2.onclick = () => numF(2)
+//button3.onclick = () => numF(3)
+//button0.onclick = () => numF(0)
